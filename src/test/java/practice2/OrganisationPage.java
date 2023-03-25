62110fe39e12514afad2d4f16b53978204fa4168
SAHIL <SAHIL@SAHIL-PC>
2023-03-25 12:20:19 +0530

    new method submmited

---
+++ src/test/java/practice2/OrganisationPage.java
@@ -0,0 +1,13 @@
+package practice2;
+
+import org.testng.annotations.Test;
+
+public class OrganisationPage {
+	
+	@Test
+	public void demoo()
+	{
+		System.out.println("Devloper devloping application");
+	}
+
+}
